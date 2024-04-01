e3467d6c6aca94ed0702ff2d0225d4bfbf9a48c0
Dorota Sn <dewsynow@gmail.com>
2024-04-01 21:18:33 +0200

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 34;
+        String name = "Dorota";
+        char surname1stLetter = 'S';
+        System.out.println("Hello World!");
+        System.out.println("Nazywam się " + name + " " + surname1stLetter + ". i mam " + age + " lata.");
+
+    }
+}
