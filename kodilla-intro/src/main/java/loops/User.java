9eec7ca1a82224850e9c178ba8755f58d32b656e
Dorota Sn <dewsynow@gmail.com>
2024-04-22 21:55:38 +0200

    [#2.5.] Divided the User class into 2 separate class: Application and User.

--- kodilla-intro/src/main/java/loops/User.java
+++ kodilla-intro/src/main/java/loops/User.java
@@ -12,44 +12,7 @@ public class User {
     public int getAge() {
         return age;
     }
-
-    public static int averageAge(User[] users) {
-        if (users.length == 0) {
-            System.out.println("There are no users to display");
-        }
-        int totalAge = 0;
-        for (int i = 0; i < users.length; i++) {
-            totalAge = totalAge + users[i].getAge();
-        }
-        return (int) totalAge / users.length;
+    public String getName(){
+        return name;
     }
-
-    public static void ageLessThanAverageAge(User[] users) {
-        int avgAge = averageAge(users);
-        if (avgAge == 0) {
-          return;
-        }
-        for (int i = 0; i < users.length; i++) {
-            if (users[i].getAge() < avgAge) {
-                System.out.println(users[i].name + " has an age below the average age of all users");
-            }
-        }
-    }
-        public static void main(String[]args){
-
-            User kasia = new User("Kasia", 38);
-            User ewa = new User("Ewa", 36);
-            User dawid = new User("Dawid", 54);
-            User kacper = new User("Kacper", 12);
-            User albert = new User("Albert", 1);
-            User alfred = new User("Alfred", 3);
-
-            User[] users = {kasia, ewa, dawid, kacper, albert, alfred};
-
-            ageLessThanAverageAge(users);
-
-        }
-    }
-
-
-
+}
